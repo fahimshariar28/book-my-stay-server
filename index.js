3aa5aa82c52e4de8f81f031206899fc47b67c28d
Fahim Shariar <fahimshariar28@gmail.com>
2023-08-26 02:50:52 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.js";
+import usersRoutes from "./routes/users.js";
+import hotelsRoutes from "./routes/hotels.js";
+import roomsRoutes from "./routes/rooms.js";
+
 const app = express();
 dotenv.config();
 const port = 5000;
@@ -26,8 +34,25 @@ mongoose.connection.on("disconnected", () => {
   console.log("Mongoose connection is disconnected");
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello Book My Stay!");
+// Middleware
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/users", usersRoutes);
+app.use("/hotels", hotelsRoutes);
+app.use("/rooms", roomsRoutes);
+
+app.use((error, req, res, next) => {
+  const status = error.status || 500;
+  const message = error.message || "Something went wrong";
+  res.status(status).json({
+    success: false,
+    status,
+    message,
+    stack: error.stack,
+  });
 });
 
 app.listen(port, () => {
